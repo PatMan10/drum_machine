a8aa5ad42d1c27da3003d33eeef4f53e62fcce35
PatMan10 <PatMan10@protonmail.com>
2019-12-13 21:31:04 +0200

    added buttons and toggles

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,152 @@ import React from "react";
 //------------UTILS-------------
 import C from "./utils/Constants";
 import F from "./utils/Functions";
+//------------UI_COMPS-------------
+import Toggle from "./ui_components/toggle/Toggle";
 
-interface State {}
+interface State {
+  isOn: boolean;
+  isPiano: boolean;
+}
 
 class App extends React.Component<{}, State> {
   constructor() {
     super({});
-    this.state = {};
+    this.state = {
+      isOn: false,
+      isPiano: false
+    };
   }
 
+  onclickButton = (e: React.MouseEvent<HTMLElement>) => {
+    switch (e.currentTarget.id) {
+      case "Q":
+        console.log("playing sound Q");
+        break;
+
+      case "W":
+        console.log("playing sound W");
+        break;
+
+      case "E":
+        console.log("playing sound E");
+        break;
+
+      case "A":
+        console.log("playing sound A");
+        break;
+
+      case "S":
+        console.log("playing sound S");
+        break;
+
+      case "D":
+        console.log("playing sound D");
+        break;
+
+      case "Z":
+        console.log("playing sound Z");
+        break;
+
+      case "X":
+        console.log("playing sound X");
+        break;
+
+      case "C":
+        console.log("playing sound C");
+        break;
+    }
+  };
+
+  togglePower = () => {
+    this.setState(curState => ({ isOn: !curState.isOn }));
+  };
+
+  toggleBank = () => {
+    this.setState(curState => ({ isPiano: !curState.isPiano }));
+  };
+
   render() {
+    const { isOn, isPiano } = this.state;
     return (
-      <main className="red-bg flex-col-aiC-jcC h-100vh">
-        <h1>Drum Machine</h1>
+      <main className="flex-col-aiC-jcC h-100vh">
+        <div id="drum-machine" className="flex-col-aiC p-05e">
+          <div id="display" className="red-bg flex-col-aiC w-100p">
+            <div className="pink-bg flex-row flex-jcSB w-40p">
+              <Toggle
+                id="power"
+                label="Power"
+                isToggled={isOn}
+                onClick={this.togglePower}
+              />
+              <Toggle
+                id="bank"
+                label="Bank"
+                isToggled={isPiano}
+                onClick={this.toggleBank}
+              />
+            </div>
+          </div>
+
+          <div id="drum-pad">
+            <input
+              id="Q"
+              value="Q"
+              type="button"
+              onClick={this.onclickButton}
+            />
+            <input
+              id="W"
+              value="W"
+              type="button"
+              onClick={this.onclickButton}
+            />
+            <input
+              id="E"
+              value="E"
+              type="button"
+              onClick={this.onclickButton}
+            />
+
+            <input
+              id="A"
+              value="A"
+              type="button"
+              onClick={this.onclickButton}
+            />
+            <input
+              id="S"
+              value="S"
+              type="button"
+              onClick={this.onclickButton}
+            />
+            <input
+              id="D"
+              value="D"
+              type="button"
+              onClick={this.onclickButton}
+            />
+
+            <input
+              id="Z"
+              value="Z"
+              type="button"
+              onClick={this.onclickButton}
+            />
+            <input
+              id="X"
+              value="X"
+              type="button"
+              onClick={this.onclickButton}
+            />
+            <input
+              id="C"
+              value="C"
+              type="button"
+              onClick={this.onclickButton}
+            />
+          </div>
+        </div>
       </main>
     );
   }
---
+++ src/ui_components/toggle/Toggle.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./toggle.css";
+
+interface Props {
+  id: string;
+  label: string;
+  onClick: () => void;
+  isToggled: boolean;
+}
+
+const Toggle: React.FC<Props> = ({ id, label, onClick, isToggled }) => {
+  let toggleClasses = "toggle toggle-s";
+  let toggleBallClasses = "red-bg toggle-ball";
+  if (isToggled) {
+    toggleClasses += " bg-primary";
+    toggleBallClasses += " flex-asE";
+  } else {
+    toggleClasses += " bg-secondary";
+  }
+  return (
+    <div className="cyan-bg toggle-container vertical">
+      <label>{label}</label>
+      <div id={id} className={toggleClasses} onClick={onClick}>
+        <div className={toggleBallClasses}></div>
+      </div>
+    </div>
+  );
+};
+
+Toggle.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  isToggled: PropTypes.bool.isRequired
+};
+
+export default Toggle;
