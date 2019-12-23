584f81611d581aadea382cd9e025412966001fef
PatMan10 <PatMan10@protonmail.com>
2019-12-23 14:15:01 +0200

    fixed audio names

--- src/app/assets/audio/heater_kit/HeaterKit.ts
+++ src/app/assets/audio/heater_kit/HeaterKit.ts
@@ -1,10 +1,10 @@
 import AudioURL from "../../../utils/classes/audio/AudioURL";
 
 export default {
-  Q: new AudioURL("Heater-1", require("./Heater-1.mp3")),
-  W: new AudioURL("Heater-2", require("./Heater-2.mp3")),
-  E: new AudioURL("Heater-3", require("./Heater-3.mp3")),
-  A: new AudioURL("Heater-4", require("./Heater-4_1.mp3")),
+  Q: new AudioURL("Heater 1", require("./Heater-1.mp3")),
+  W: new AudioURL("Heater 2", require("./Heater-2.mp3")),
+  E: new AudioURL("Heater 3", require("./Heater-3.mp3")),
+  A: new AudioURL("Heater 4", require("./Heater-4_1.mp3")),
   S: new AudioURL("Clap", require("./Heater-6.mp3")),
   D: new AudioURL("Open HH", require("./Dsc_Oh.mp3")),
   Z: new AudioURL("Kick n' Hat", require("./Kick_n_Hat.mp3")),
--- src/app/assets/audio/piano_kit/PianoKit.ts
+++ src/app/assets/audio/piano_kit/PianoKit.ts
@@ -1,9 +1,9 @@
 import AudioURL from "../../../utils/classes/audio/AudioURL";
 
 export default {
-  Q: new AudioURL("Chord-1", require("./Chord_1.mp3")),
-  W: new AudioURL("Chord-2", require("./Chord_2.mp3")),
-  E: new AudioURL("Chord-3", require("./Chord_3.mp3")),
+  Q: new AudioURL("Chord 1", require("./Chord_1.mp3")),
+  W: new AudioURL("Chord 2", require("./Chord_2.mp3")),
+  E: new AudioURL("Chord 3", require("./Chord_3.mp3")),
   A: new AudioURL("Shaker", require("./Give_us_a_light.mp3")),
   S: new AudioURL("Open HH", require("./Dry_Ohh.mp3")),
   D: new AudioURL("Closed HH", require("./Bld_H1.mp3")),
